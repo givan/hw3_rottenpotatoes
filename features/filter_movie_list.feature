ead8f7780105a5ab6a08fb97d3107f4ee3a65133
George Ivanov <george.ivanov@hotmail.com>
2012-10-28 18:29:22 -0700

    Filtering scenarios implemented and passing

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -22,14 +22,18 @@ Background: movies have been added to database
   And  I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  When I check the following ratings: PG, R
+    And I press "Refresh"
+  Then I should only see movies with these ratings 
 
 Scenario: no ratings selected
-  # see assignment
+  When I check the following ratings: PG, R
+    And I press "Refresh"
+    And I uncheck all ratings
+    And I press "Refresh" 
+  Then I should only see movies with ratings: PG, R
 
 Scenario: all ratings selected
-  # see assignment
+  When I check all ratings
+    And I press "Refresh"
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,9 +1,49 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  @all_movies = Array.new
   movies_table.hashes.each do |movie|
-    Movie.create(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
+    @new_movie = Movie.create(:title => movie[:title], :rating => movie[:rating], :release_date => movie[:release_date])
+    @all_movies << @new_movie
   end
+
+  @all_ratings = @all_movies.collect { |movie| movie.rating }.uniq
+end
+
+When /^I check all ratings$/ do
+   @expected_ratings = @all_ratings
+
+   step "I check expected ratings"
+end
+
+When /^I uncheck all ratings$/ do
+   @expected_ratings = []
+
+   # this will uncheck all ratings
+   step "I check expected ratings"
+end
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  @expected_ratings = Array.new
+  rating_list.split(',').each do |rating|
+       rating.strip!
+       @expected_ratings << rating
+  end
+
+  step "I check expected ratings"
+end
+
+When /^I check expected ratings$/ do
+  @all_ratings.each do |rating|
+      if @expected_ratings.include?(rating) then
+         step "I check \"ratings_#{rating}\""
+      else
+         step "I uncheck \"ratings_#{rating}\""
+      end
+  end 
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -15,12 +55,29 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+Then /^I should only see movies with these ratings$/ do
+   # debugger
+   @all_movies.each do |movie| 
+      if @expected_ratings.include?(movie.rating) then
+         step "I should see \"#{movie.title}\""
+      else
+         step "I should not see \"#{movie.title}\""
+      end
+   end
 end
+
+Then /^I should only see movies with ratings: (.*)/ do |rating_list|
+  @expected_ratings = Array.new
+  rating_list.split(',').each do |rating|
+       rating.strip!
+       @expected_ratings << rating
+  end
+
+  step "I should only see movies with these ratings"
+end
+
+Then /^I should see all of the movies$/ do
+  step "I should only see movies with these ratings"  
+end
+
+
